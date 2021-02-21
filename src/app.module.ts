40c258560cd7e8dfedaa6a07b7b53e0880ccdc19
Kamil Kowalczuk-Fijałkowski <Kamil.Kowalczuk-Fijalkowski@grupawp.pl>
2021-02-21 14:33:09 +0100

    feat/guards add guards with all models data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,14 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InvoicesController } from './invoices/invoices.controller';
 import { InvoicesService } from './invoices/invoices.service';
 import { InvoicesModule } from './invoices/invoices.module';
-import { BodyMiddleware } from './invoices/body.middleware';
+import { SummaryService } from './summary/summary.service';
 
 @Module({
   imports: [InvoicesModule],
   controllers: [AppController, InvoicesController],
-  providers: [AppService, InvoicesService],
+  providers: [AppService, InvoicesService, SummaryService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(BodyMiddleware).forRoutes('invoices/add');
-  }
-}
+export class AppModule {}
