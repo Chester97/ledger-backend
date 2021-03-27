bf56f81fcc23e21d64ffb03a6faf1bbaeb7e9676
Kamil Kowalczuk-Fijałkowski <Kamil.Kowalczuk-Fijalkowski@grupawp.pl>
2021-03-27 14:28:47 +0100

    feat: invoiceClear

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { InvoicesController } from './invoices/invoices.controller';
-import { InvoicesService } from './invoices/invoices.service';
+import { ConfigModule } from '@nestjs/config';
 import { InvoicesModule } from './invoices/invoices.module';
-import { SummaryService } from './summary/summary.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
     InvoicesModule,
     MongooseModule.forRoot(
-      'mongodb+srv://user_test:narusaku404@cluster.ragh7.mongodb.net/ledger',
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster.ragh7.mongodb.net/ledger`,
       {
         useNewUrlParser: true,
         useFindAndModify: false,
